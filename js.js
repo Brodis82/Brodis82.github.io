1cc7449c3ca1be5a636159518d36c4425a8f831e
Brodis82 <brodis82@hotmail.com>
2023-10-07 23:21:48 -0500

    cambio

--- js.js
+++ js.js
@@ -1,8 +1,6 @@
 // Obtenemos el canvas y su contexto
 var canvas = document.getElementById('miCanvas');
 var ctx = canvas.getContext('2d');
-var rango = document.getElementById('rango');
-
 
 // Configuramos el color y el tamaño del pincel
 var colorSelector = document.getElementById('colorSelector');
@@ -30,8 +28,15 @@ function dibujar(e) {
 
     // Calcular las coordenadas en función del tamaño real del canvas
     var rect = canvas.getBoundingClientRect();
-    var x = (e.clientX - rect.left) * (canvas.width / rect.width);
-    var y = (e.clientY - rect.top) * (canvas.height / rect.height);
+    var x, y;
+
+    if (e.type === 'touchmove' || e.type === 'touchstart') {
+        x = (e.touches[0].clientX - rect.left) * (canvas.width / rect.width);
+        y = (e.touches[0].clientY - rect.top) * (canvas.height / rect.height);
+    } else {
+        x = (e.clientX - rect.left) * (canvas.width / rect.width);
+        y = (e.clientY - rect.top) * (canvas.height / rect.height);
+    }
 
     ctx.lineTo(x, y);
     ctx.stroke();
@@ -56,22 +61,14 @@ canvas.addEventListener('mouseup', function() {
 canvas.addEventListener('touchstart', function(e) {
     dibujando = true;
     ctx.beginPath();
-    var touch = e.touches[0];
-    var rect = canvas.getBoundingClientRect();
-    var x = (touch.clientX - rect.left) * (canvas.width / rect.width);
-    var y = (touch.clientY - rect.top) * (canvas.height / rect.height);
-    ctx.moveTo(x, y);
+    e.preventDefault();
+    dibujar(e);
 });
 
 canvas.addEventListener('touchmove', function(e) {
     if (!dibujando) return;
     e.preventDefault();
-    var touch = e.touches[0];
-    var rect = canvas.getBoundingClientRect();
-    var x = (touch.clientX - rect.left) * (canvas.width / rect.width);
-    var y = (touch.clientY - rect.top) * (canvas.height / rect.height);
-    ctx.lineTo(x, y);
-    ctx.stroke();
+    dibujar(e);
 });
 
 canvas.addEventListener('touchend', function() {
@@ -82,14 +79,14 @@ canvas.addEventListener('touchend', function() {
 // Función para actualizar el label con el valor actual del rango
 function actualizarTamanoLabel() {
     tamanoLabel.textContent = 'Tamaño del pincel: ' + tamanoSelector.value;
-  }
-  
-  // Agregamos un evento al input range para actualizar el label cuando cambia el valor
-  tamanoSelector.addEventListener('input', actualizarTamanoLabel);
-  
-  // Llamamos a la función inicialmente para establecer el valor predeterminado
-  actualizarTamanoLabel();
-  
+}
+
+// Agregamos un evento al input range para actualizar el label cuando cambia el valor
+tamanoSelector.addEventListener('input', actualizarTamanoLabel);
+
+// Llamamos a la función inicialmente para establecer el valor predeterminado
+actualizarTamanoLabel();
+
 // Función para limpiar el canvas
 function limpiarCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
